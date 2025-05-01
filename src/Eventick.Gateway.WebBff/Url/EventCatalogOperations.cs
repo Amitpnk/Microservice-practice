e3cb677d3918190eb2b8bd7ced8e6d608197b055
Amit P Naik <amit.naik8103@gmail.com>
2025-05-01 22:24:54 +0530

    feat: adding sln file

--- src/Eventick.Gateway.WebBff/Url/EventCatalogOperations.cs
+++ src/Eventick.Gateway.WebBff/Url/EventCatalogOperations.cs
@@ -6,6 +6,6 @@ public class EventCatalogOperations
 {
     public static string GetEventsPerCategory(Guid categoryId) => $"/api/events?categoryid={categoryId}";
     public static string GetEventById(Guid id) => $"/api/events/{id}";
-    public static string GetAllcategories() => "/api/categories";
+    public static string GetAllcategories() => "/api/category";
     public static string GetAllEvents() => "/api/events";
 }
