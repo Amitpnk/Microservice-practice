7cf5ca59ca584b692b86f5abec4fcbb06ef71d74
Amit P Naik <amit.naik8103@gmail.com>
2025-05-01 22:24:21 +0530

    feat: adding client

---
+++ src/Eventick.Client/Models/View/OrderViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Eventick.Web.Models.Api;
+using System.Collections.Generic;
+
+namespace Eventick.Web.Models.View
+{
+    public class OrderViewModel
+    {
+        public IEnumerable<Order> Orders { get; set; }
+    }
+}
---
+++ src/Eventick.Client/Services/EventCatalogService.cs
@@ -0,0 +1,44 @@
+﻿using Eventick.Web.Extensions;
+using Eventick.Web.Models.Api;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Eventick.Web.Services
+{
+    public class EventCatalogService : IEventCatalogService
+    {
+        private readonly HttpClient client;
+
+        public EventCatalogService(HttpClient client)
+        {
+            this.client = client;
+        }
+
+        public async Task<IEnumerable<Event>> GetAll()
+        {
+            var response = await client.GetAsync("/api/event");
+            return await response.ReadContentAs<List<Event>>();
+        }
+
+        public async Task<IEnumerable<Event>> GetByCategoryId(Guid categoryid)
+        {
+            var response = await client.GetAsync($"/api/event/?categoryId={categoryid}");
+            return await response.ReadContentAs<List<Event>>();
+        }
+
+        public async Task<Event> GetEvent(Guid id)
+        {
+            var response = await client.GetAsync($"/api/event/{id}");
+            return await response.ReadContentAs<Event>();
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            var response = await client.GetAsync("/api/category");
+            return await response.ReadContentAs<List<Category>>();
+        }
+
+    }
+}
